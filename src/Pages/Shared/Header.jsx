e2f6b1e2a836bb907adfc57f9605de1d18398552
Barshon <barshonweb@gmail.com>
2023-11-19 16:44:26 +0600

    basic routes and page setup

---
+++ src/Pages/Shared/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <p>s</p>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { RouterProvider } from "react-router-dom";
+import router from './routes/Routes';
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
