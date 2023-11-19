df43b2bd02596146a05244b053ae3f73a23be6c8
Barshon <barshonweb@gmail.com>
2023-11-20 01:25:14 +0600

    404 page created (responsive)

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-      <div className="flex items-center md:px-14 px-10 md:mx-10 mx-5 mt-20">
-        <h3 className="md:text-2xl text-lg p-5 bg-slate-200 rounded-xl">
+      <div className="md:flex items-center md:px-14 px-10 md:mx-10 mx-5 mt-20">
+        <h3 className="md:text-2xl text-lg px-2 py-2 md:p-5 bg-slate-200 rounded-xl">
           <span className="md:text-5xl text-red-600">404! Not found!</span> But no worries,
           the friendly neighbourhood spiderman will
           <Link to='/'>
-            <button className="btn btn-info mt-4">swing you back home!</button>
+            <button className="btn btn-info ms-4 md:ms-0 md:mt-4">swing you back home!</button>
           </Link>
         </h3>
         <img src="../../404.png" alt="" />
