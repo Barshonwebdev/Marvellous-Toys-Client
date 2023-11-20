bba2a4bfb3d80006b0d2f7eb214510ee3426d008
Barshon <barshonweb@gmail.com>
2023-11-20 13:00:34 +0600

    404 page created (responsive)

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
       <div className="md:flex items-center md:px-14 px-10 md:mx-10 mx-5 mt-20">
         <h3 className="md:text-2xl text-lg px-2 py-2 md:p-5 bg-slate-200 rounded-xl">
           <span className="md:text-5xl text-red-600">404! Not found!</span> But no worries,
-          the friendly neighbourhood spiderman will
+          your friendly neighbourhood spiderman will
           <Link to='/'>
             <button className="btn btn-info ms-4 md:ms-0 md:mt-4">swing you back home!</button>
           </Link>
