e333d8c3ab1181538234fa45a57c560cfbe85f30
Barshon <barshonweb@gmail.com>
2023-11-20 13:38:35 +0600

    link routes

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Header.css'
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
@@ -27,7 +28,9 @@ const Header = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a className="header-font">Home</a>
+                <Link to="/" className="header-font">
+                  Home
+                </Link>
               </li>
               <li>
                 <a className="header-font">All Figurines</a>
@@ -40,31 +43,28 @@ const Header = () => {
               </li>
 
               <li>
-                <a className="header-font">Blogs</a>
+                <Link to="/blogs" className="header-font">
+                  Blogs
+                </Link>
               </li>
             </ul>
           </div>
           <img className="w-72 animate-pulse" src="../../../logo.png" alt="" />
         </div>
         <div className="navbar-center hidden lg:flex space-x-6 font-bold text-2xl text-slate-700">
-          
-          
-              <a className="header-font">Home</a>
-            
-            
-              <a className="header-font">All Figurines</a>
-            
-            
-              <a className="header-font">My Figurines</a>
-            
-          
-              <a className="header-font">Add a Figurine</a>
-            
+          <Link to="/" className="header-font">
+            Home
+          </Link>
 
-            
-              <a className="header-font">Blogs</a>
-            
-          
+          <a className="header-font">All Figurines</a>
+
+          <a className="header-font">My Figurines</a>
+
+          <a className="header-font">Add a Figurine</a>
+
+          <Link to="/blogs" className="header-font">
+            Blogs
+          </Link>
         </div>
         <div className="navbar-end">
           <a className="btn ">Login</a>
