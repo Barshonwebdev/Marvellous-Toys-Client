99352ee5896522baa6c0c7443e1a215c0723ed61
Barshon <barshonweb@gmail.com>
2023-11-23 21:01:38 +0600

    delete implemented in my figurine

--- src/Pages/MyFigurines.jsx
+++ src/Pages/MyFigurines.jsx
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProvider";
 import { FaPen } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
+import toast, { Toaster } from "react-hot-toast";
+const notify = () => toast("Figurine Deleted Successfully!");
 
 const MyFigurines = () => {
   const [myfigures, setMyfigures] = useState([]);
@@ -17,8 +19,22 @@ const MyFigurines = () => {
       .then((data) => setMyfigures(data));
   }, [asc]);
 
-  const handleAsc=()=>{
-
+  const handleDelete=(id)=>{
+    const proceed= confirm("Are you sure you want to delete this figurine of yours?");
+    if(proceed){
+        fetch(`http://localhost:5000/my/${id}`,{
+            method:"DELETE",
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.deletedCount>0){
+                notify();
+                const remaining=myfigures.filter((myfigure)=>myfigure._id!==id);
+                setMyfigures(remaining);
+            }
+        })
+    }
   }
   return (
     <div>
@@ -29,8 +45,13 @@ const MyFigurines = () => {
       <div className="overflow-x-auto mt-10">
         <div className="flex justify-center mb-5 md:justify-end space-x-5 items-center me-10">
           <small className="text-md font-bold"> showing by price:</small>
-          
-          <button onClick={()=>setAsc(!asc)} className="btn text-white hover:bg-slate-700 bg-orange-900 btn-sm">{asc? "Low to high" : "High to low"}</button>
+
+          <button
+            onClick={() => setAsc(!asc)}
+            className="btn text-white hover:bg-slate-700 bg-orange-900 btn-sm"
+          >
+            {asc ? "Low to high" : "High to low"}
+          </button>
         </div>
         <table className="table table-xs md:table-md">
           {/* head */}
@@ -67,12 +88,15 @@ const MyFigurines = () => {
                   </Link>
                 </td>
                 <td>
-                  <button className="btn-info btn-sm btn">
+                  <button className="btn-info btn-sm btn hover:bg-teal-800">
                     <FaPen className="text-white"></FaPen>
                   </button>
                 </td>
                 <td>
-                  <button className="btn-error btn-sm btn">
+                  <button
+                    onClick={() => handleDelete(figure._id)}
+                    className="btn-error btn-sm btn hover:bg-red-800"
+                  >
                     <FaTrashAlt className="text-white"></FaTrashAlt>
                   </button>
                 </td>
@@ -81,6 +105,16 @@ const MyFigurines = () => {
           </tbody>
         </table>
       </div>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            border: "2px solid #713200",
+
+            color: "red",
+          },
+        }}
+      ></Toaster>
     </div>
   );
 };
